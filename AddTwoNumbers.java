ff35914e0973cc995bcb5e57fe87320c8f58a1c2
Sri-Varshini1125 <srivarshini1125@gmail.com>
2025-12-26 23:20:45 +0530

    Add files via upload

---
+++ AddTwoNumbers.java
@@ -0,0 +1,11 @@
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+        int sum;
+
+        sum = num1 + num2;
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
