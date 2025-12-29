2beca99380abdf7935f67f0d09aac2a1e36f0729
Sri-Varshini1125 <srivarshini1125@gmail.com>
2025-12-29 12:35:43 +0530

    Add files via upload

---
+++ AllDataTypes.java
@@ -0,0 +1,20 @@
+public class AllDataTypes {
+    public static void main(String[] args) {
+        byte smallNum = 127;               
+        short midNum = 32000;              
+        int standardInt = 2147483647;       
+        long largeNum = 9223372036854775807L; 
+        float decimalFloat = 3.14f;      
+        double decimalDouble = 3.14159265;  
+        char grade = 'A';                   
+        boolean isJavaFun = true;                    
+        System.out.println("Byte: " + smallNum);
+        System.out.println("Short: " + midNum);
+        System.out.println("Int: " + standardInt);
+        System.out.println("Long: " + largeNum);
+        System.out.println("Float: " + decimalFloat);
+        System.out.println("Double: " + decimalDouble);
+        System.out.println("Char: " + grade);
+        System.out.println("Boolean: " + isJavaFun);
+    }
+}
