de4cc7c28fca1de3a0724cb6698aa04b25707376
Sri-Varshini1125 <srivarshini1125@gmail.com>
2025-12-30 21:50:38 +0530

    Add files via upload

---
+++ ArithmeticOperatorsExample.java
@@ -0,0 +1,19 @@
+public class ArithmeticOperatorsExample {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 3;
+
+        // Addition
+        System.out.println("Addition (a + b): " + (a + b));
+
+        // Subtraction
+        System.out.println("Subtraction (a - b): " + (a - b));
+
+        // Multiplication
+        System.out.println("Multiplication (a * b): " + (a * b));
+
+        // Division
+        System.out.println("Division (a / b): " + (a / b));
+    }
+}
