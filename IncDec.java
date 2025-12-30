495e0d499d72a20d0807d40a02e59e3a187e066f
Sri-Varshini1125 <srivarshini1125@gmail.com>
2025-12-30 21:54:18 +0530

    Add files via upload

---
+++ IncDec.java
@@ -0,0 +1,10 @@
+public class IncDec {
+    public static void main(String[] args) {
+
+        int num = 10;
+
+        num++;          System.out.println("After increment: " + num);
+
+        num--;           System.out.println("After decrement: " + num);
+    }
+}
