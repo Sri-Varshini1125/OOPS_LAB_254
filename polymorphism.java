734e8188ed517ba09929b249592bcd9e3425f77b
Sri-Varshini1125 <srivarshini1125@gmail.com>
2026-03-23 12:13:40 +0530

    Add files via upload

---
+++ polymorphism.java
@@ -0,0 +1,21 @@
+class Parent {
+    void view() {
+        System.out.println("This is a parent class method");
+    }
+}
+
+class Child extends Parent {
+    void view() {
+        System.out.println("This is a child class method");
+    }
+}
+
+public class polymorphism {
+    public static void main(String args[]) {
+        Parent ob = new Parent();
+        ob.view();
+
+        Child ob1 = new Child();
+        ob1.view();
+    }
+}
